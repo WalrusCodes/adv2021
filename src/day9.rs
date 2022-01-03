526d943dcc58d4380f6fca49ee5ab26dd5e96f00
walruscodes <walruscodes@users.noreply.github.com>
2022-01-02 21:47:44 -0800

    day 9, part 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,16 +1,6 @@
-fn main() {
-    let path = std::env::args().nth(1).expect("pls provide input file");
-    let contents = std::fs::read_to_string(path).expect("read failed");
+use std::collections::{HashSet, VecDeque};
 
-    let grid: Vec<_> = contents
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|c| c.to_digit(10).unwrap() as usize)
-                .collect::<Vec<usize>>()
-        })
-        .collect();
-
+fn part1(grid: &Vec<Vec<usize>>) {
     let row_count = grid.len();
     let col_count = grid[0].len();
 
@@ -37,4 +27,73 @@ fn main() {
         }
     }
     dbg!(risk_level_sum);
+}
+
+fn part2(grid: &Vec<Vec<usize>>) -> usize {
+    // (row, col)
+    let mut seen = HashSet::<(usize, usize)>::new();
+    let mut sizes = Vec::<usize>::new();
+
+    let row_count = grid.len();
+    let col_count = grid[0].len();
+
+    for row in 0..row_count {
+        for col in 0..col_count {
+            if seen.contains(&(row, col)) {
+                continue;
+            }
+            seen.insert((row, col));
+
+            let mut queue = VecDeque::<(usize, usize)>::new();
+            queue.push_back((row, col));
+            let mut size = 0;
+            while !queue.is_empty() {
+                let (row, col) = queue.pop_front().unwrap();
+                if grid[row][col] == 9 {
+                    continue;
+                }
+                size += 1;
+                let mut consider: Vec<(usize, usize)> = Vec::new();
+                if row != 0 {
+                    consider.push((row - 1, col));
+                }
+                if row != (row_count - 1) {
+                    consider.push((row + 1, col));
+                }
+                if col != 0 {
+                    consider.push((row, col - 1));
+                }
+                if col != (col_count - 1) {
+                    consider.push((row, col + 1));
+                }
+                for row_col in consider {
+                    if !seen.contains(&row_col) {
+                        seen.insert(row_col);
+                        queue.push_back(row_col);
+                    }
+                }
+            }
+            sizes.push(size);
+        }
+    }
+    sizes.sort();
+    // dbg!(sizes);
+    sizes[sizes.len() - 1] * sizes[sizes.len() - 2] * sizes[sizes.len() - 3]
+}
+
+fn main() {
+    let path = std::env::args().nth(1).expect("pls provide input file");
+    let contents = std::fs::read_to_string(path).expect("read failed");
+
+    let grid: Vec<_> = contents
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect::<Vec<usize>>()
+        })
+        .collect();
+
+    part1(&grid);
+    dbg!(part2(&grid));
 }
